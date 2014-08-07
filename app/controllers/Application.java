a3744f2df3775f13b5ea6c11ded62abbae10dc84
Mariusz <mnowicki@sosoftware.plgit initgit config --global user.email mnowicki@sosoftware.pl>
2014-08-07 10:08:03 +0200

    config

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,13 +7,8 @@ import models.IsaaCloudAPI;
 import models.Users;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
 
 public class Application extends Controller {
-
-	public static Result index() {
-		return ok(index.render("Your new application is ready"));
-	}
 
 	public static Result deleteEvent() {
 		GoogleCalendarAPI calendar = new GoogleCalendarAPI();
