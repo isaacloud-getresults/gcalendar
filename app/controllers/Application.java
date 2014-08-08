b1f06bcaac38b211ac98bf1b52ff451be85f90a8
Mariusz <mnowicki@sosoftware.plgit initgit config --global user.email mnowicki@sosoftware.pl>
2014-08-08 15:59:21 +0200

    test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,50 +1,26 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import models.GoogleCalendarAPI;
 import models.IsaaCloudAPI;
-import models.SOICalendar;
 import models.Users;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
 public class Application extends Controller {
-
-	public static SOICalendar soiCalendar;
-
-	private static final String APPLICATION_NAME = "SOI Calendar";
-
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(
-			System.getProperty("user.home"), ".store/Calendars");
-
-	private static FileDataStoreFactory dataStoreFactory;
-
-	private static HttpTransport httpTransport;
-
-	private static final JsonFactory JSON_FACTORY = JacksonFactory
-			.getDefaultInstance();
-
-	public static com.google.api.services.calendar.Calendar service;
 
 	public static Result deleteEvent() {
 		GoogleCalendarAPI calendar = new GoogleCalendarAPI();
@@ -94,41 +70,37 @@ public class Application extends Controller {
 		return ok("" + xx);
 	}
 
-	private static Credential authorize() throws Exception {
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-				JSON_FACTORY,
-				new InputStreamReader(GoogleCalendarAPI.class
-						.getResourceAsStream("/client_secrets.json")));
-
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, JSON_FACTORY, clientSecrets,
-				Collections.singleton(CalendarScopes.CALENDAR))
-				.setDataStoreFactory(dataStoreFactory).build();
-		return new AuthorizationCodeInstalledApp(flow,
-				new LocalServerReceiver()).authorize("user");
-	}
-
 	public static Result a() {
 		Events events = null;
 		String x = "ss ";
+
 		try {
-			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+			GoogleCredential credential = new GoogleCredential.Builder()
+					.setTransport(GoogleNetHttpTransport.newTrustedTransport())
+					.setJsonFactory(new GsonFactory())
+					.setServiceAccountId(
+							"<service account email address>@developer.gserviceaccount.com")
+					.setServiceAccountScopes(
+							Arrays.asList("https://www.googleapis.com/auth/calendar"))
+					.setServiceAccountPrivateKeyFromP12File(
+							new File(
+									"<private key for service account in P12 format>-privatekey.p12"))
+					.setServiceAccountUser(
+							"<domain user whose data you need>@yourdomain.com")
+					.build();
 
-			dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+			com.google.api.services.calendar.Calendar service = new Calendar.Builder(
+					GoogleNetHttpTransport.newTrustedTransport(),
+					new GsonFactory(), credential).build();
 
-			Credential credential = authorize();
-
-			service = new com.google.api.services.calendar.Calendar.Builder(
-					httpTransport, JSON_FACTORY, credential)
-					.setApplicationName(APPLICATION_NAME).build();
-
-			soiCalendar = new SOICalendar();
 			String pageToken = null;
 			events = service.events().list("primary").setPageToken(pageToken)
 					.execute();
+
 		} catch (IOException e) {
 		} catch (Throwable t) {
 		}
+
 		List<Event> myEvents = events.getItems();
 		for (Event event : myEvents) {
 			if (event.getLocation() != null && event.getAttendees() != null) {
@@ -139,10 +111,5 @@ public class Application extends Controller {
 		}
 
 		return ok(x);
-	}
-
-	public static Result b() {
-
-		return ok("b");
 	}
 }
