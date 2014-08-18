7e8c0290e2dd8fbb29c51bbf7862f2795890d2cd
Mariusz <mnowicki@sosoftware.plgit initgit config --global user.email mnowicki@sosoftware.pl>
2014-08-18 16:46:36 +0200

    Isaa notifications

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,7 +26,8 @@ public class Application extends Controller {
 		IsaaCloudAPI isaa = new IsaaCloudAPI();
 
 		// dostaje skądś maila (ISAACLOUD) że pojawił się w meeting room.
-		String a = "" + request().body().asJson().get("body").toString();
+		String a = ""
+				+ request().body().asJson().get("body").get("data").asText();
 
 		String userEmail = "mnowicki@sosoftware.pl";
 
