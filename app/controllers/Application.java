827a99d75188a31aeabeeed63a856df1dcbb2ccf
Mariusz <mnowicki@sosoftware.plgit initgit config --global user.email mnowicki@sosoftware.pl>
2014-08-18 15:02:24 +0200

    Isaa notifications

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -31,6 +31,7 @@ public class Application extends Controller {
 
 		// if (calendar.soiCalendar.checkCalendarMeetings(calendar.service,
 		// userEmail))
+		userEmail = "mnowicki@gmail.com";
 		isaa.addPointsForAttendance(userEmail);
 
 		return ok("ok");
