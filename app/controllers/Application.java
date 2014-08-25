f807365b56b8a92791787417090914ca48c408cb
Mariusz <mnowicki@sosoftware.plgit initgit config --global user.email mnowicki@sosoftware.pl>
2014-08-25 11:15:47 +0200

    update

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -43,6 +43,7 @@ public class Application extends Controller {
 	public static Result meetingCheck() {
 		String userEmail = ""
 				+ request().body().asJson().get("body").get("data").asText();
+		System.out.println(userEmail);
 		String isaaBase64 = ""
 				+ request().body().asJson().get("body").get("isaaBase64")
 						.asText();
